d0128410f502c004e704635937c482f34d5a5f23
AMN-SHK <hmsamaan@gmail.com>
2021-12-18 13:04:30 +0530

    Admin Login page bg

--- frontend/src/components/adminpanel/loginpage.js
+++ frontend/src/components/adminpanel/loginpage.js
@@ -1,54 +1,56 @@
 import React, { Component } from 'react'
 import {Redirect} from 'react-router-dom'
+import "../../css/admin_login_jay.css";
+
 class Loginpage extends Component {
+  state = {
+    username: "",
+    password: "",
+  };
 
-    state = {
-        username:'',
-        password:''
-    }
+  setUserName = (u) => {
+    this.setState({
+      username: u,
+    });
+  };
+  setPassword = (p) => {
+    this.setState({
+      password: p,
+    });
+  };
 
-    setUserName = (u) =>{
-        this.setState({
-            username:u
-        })
-    }
-    setPassword = (p) =>{
-        this.setState({
-            password:p
-        })
-    }
+  sendLoginInfo = (e) => {
+    e.preventDefault();
+    this.props.history.push("/admin_welcome");
+  };
 
-    sendLoginInfo = (e) =>{
-        e.preventDefault();
-    this.props.history.push('/admin_welcome')
-            
-        
-    }
-
-    render() {
-        return (
-            <div>
-
-                <div>
-                    <h1>Login</h1>
-                    <p>Sign in and start managing your leaderboard</p>
-                </div>
-                <form onSubmit={this.sendLoginInfo}>
-                    <label>
-                        Username
-                    </label>
-                    <input type = "text" value={this.state.username} onChange={(e)=>this.setUserName(e.target.value)}/>
-                    <br/>
-                    <label>
-                        password
-                    </label>
-                    <input type = "password" value={this.state.password} onChange={(e)=>this.setPassword(e.target.value)}/>
-                    <br/>
-                    <button type="submit">Login</button>
-                </form>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="admin_panel_login_main">
+        <div>
+          <h1>Login</h1>
+          <p>Sign in and start managing your leaderboard</p>
+        </div>
+        <form onSubmit={this.sendLoginInfo}>
+          <label>Username</label>
+          <input
+            type="text"
+            value={this.state.username}
+            onChange={(e) => this.setUserName(e.target.value)}
+          />
+          <br />
+          <label>password</label>
+          <input
+            type="password"
+            value={this.state.password}
+            onChange={(e) => this.setPassword(e.target.value)}
+          />
+          <br />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Loginpage
