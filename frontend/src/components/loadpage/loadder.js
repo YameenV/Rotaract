548ccc00b4711b9f99405e341fe1260ce43a6ba6
Abhinav pandey <pandeysandeep1190@gmail.com>
2022-01-02 19:20:17 +0530

    video

--- frontend/src/components/loadpage/loadder.js
+++ frontend/src/components/loadpage/loadder.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
-
+import AnnVideo from '../../background/announcements.mp4'
 export default class Loader extends Component {
 
     render() {
         return (
             <div>
+
+<div>
+<video autoPlay muted loop
+      style={{
+        position:"absolute",
+        objectFit:"cover",
+        width:"100%",
+        height:"100%",
+        zIndex:"-1",
+        position:"fixed",
+      }}
+      >
+			<source src={AnnVideo} type="video/mp4" />
+      </video>
+</div>
+
                 {
                     setTimeout(()=>{
                         this.props.history.push('/home')
