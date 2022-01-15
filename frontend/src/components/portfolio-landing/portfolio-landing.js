10aee58a50463354363c9485e19e2184cd3d8413
Ashutosh-upadhya <ashutosh.aku.aau@gmail.com>
2022-01-15 20:34:43 +0530

    member landing + 2 video replaced with gif

--- frontend/src/components/portfolio-landing/portfolio-landing.js
+++ frontend/src/components/portfolio-landing/portfolio-landing.js
@@ -1,51 +1,96 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 import "../../css/portfolio-landing.css";
 import ashu from "../../imgs/god/ashutosh.jpeg";
 import astro from "../../imgs/astro.png";
-import {  ButtonGroup, InputGroup, FormControl } from "react-bootstrap";
-// import navbar from "../"
-
+import { ButtonGroup, InputGroup, FormControl } from "react-bootstrap";
+import Navbar from "../navbar/navbar"
 
 const Landing = () => {
-
-  const [name,SetName] = useState('');
+  const [name, SetName] = useState("");
 
   let data = [
     {
-      img:ashu,
-      membername:'Rtr. Ashutosh Upadhyay',
-      status:'President'
+      img: ashu,
+      membername: "Rtr. Ashutosh Upadhyay",
+      status: "President",
     },
     {
-      img:ashu,
-      membername:'Amaan shaikh',
-      status:'President'
+      img: ashu,
+      membername: "Amaan shaikh",
+      status: "President",
     },
     {
-      img:ashu,
-      membername:'Aakash Varma',
-      status:'President'
+      img: ashu,
+      membername: "Aakash Varma",
+      status: "President",
     },
     {
-      img:ashu,
-      membername:'Sanloni sharma',
-      status:'President'
+      img: ashu,
+      membername: "Sanloni sharma",
+      status: "President",
     },
     {
-      img:ashu,
-      membername:'Rajesh ulhas',
-      status:'President'
-    }
-  ]
+      img: ashu,
+      membername: "Rajesh ulhas",
+      status: "President",
+    },
+    {
+      img: ashu,
+      membername: "Rajesh ulhas",
+      status: "President",
+    },
+    {
+      img: ashu,
+      membername: "Rajesh ulhas",
+      status: "President",
+    },
+    {
+      img: ashu,
+      membername: "Rajesh ulhas",
+      status: "President",
+    },
+    {
+      img: ashu,
+      membername: "Rajesh ulhas",
+      status: "President",
+    },
+    {
+      img: ashu,
+      membername: "Rajesh ulhas",
+      status: "President",
+    },
+    {
+      img: ashu,
+      membername: "Rajesh ulhas",
+      status: "President",
+    },
+    {
+      img: ashu,
+      membername: "Rajesh ulhas",
+      status: "President",
+    },
+    {
+      img: ashu,
+      membername: "Rajesh ulhas",
+      status: "President",
+    },
+    {
+      img: ashu,
+      membername: "Rajesh ulhas",
+      status: "President",
+    },
+    {
+      img: ashu,
+      membername: "Rajesh ulhas",
+      status: "President",
+    },
+  ];
 
   return (
     <div className="main-11">
-      
+      <Navbar flags={1} />
       <div className="landing-header">
-<<<<<<< HEAD
-        {/* <img  className="landing-bg-astro" src={astro}></img> */}
-=======
->>>>>>> 978f7ac326a341d524fa6d17c39776d4423ab915
+        
         <h1> Rotracter 2021-2022</h1>
         <div className="member-landing_search">
           <InputGroup>
@@ -53,36 +98,35 @@ const Landing = () => {
               className="memberlanding_inputfield"
               placeholder="Search Rotracter"
               aria-label="Username"
-              value = {name}
-              onChange= {(e)=>SetName(e.target.value)}
+              value={name}
+              onChange={(e) => SetName(e.target.value)}
             />
           </InputGroup>
         </div>
       </div>
       <div className="landing-containers">
-        {
-          data.filter((item)=>{
-            if(item.membername===""){
-              return item
+        {data
+          .filter((item) => {
+            if (item.membername === "") {
+              return item;
+            } else if (
+              item.membername.toLowerCase().includes(name) ||
+              item.membername.includes(name)
+            ) {
+              return item;
             }
-            else if(item.membername.toLowerCase().includes(name) || item.membername.includes(name)){
-              return item
-            }
-          }).map((member,i)=>{
-            return(
-<div className="member-div-1" key = {i}>
-            <div>
-              <img src={member.img} className="member-div-1-image"></img>
-            </div>
-            <div className="member-div-1-name">{member.membername}</div>
-            <div className="member-div-1-status">{member.status}</div>
-          </div>
-            )
-            
           })
-        }
-       
-        
+          .map((member, i) => {
+            return (
+              <div className="member-div-1" key={i}>
+                <div className="member-div-1-div">
+                  <img src={member.img} className="member-div-1-image"></img>
+                </div>
+                <div className="member-div-1-name">{member.membername}</div>
+                <div className="member-div-1-status">{member.status}</div>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
