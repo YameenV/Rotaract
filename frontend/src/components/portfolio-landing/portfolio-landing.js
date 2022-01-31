b2a9287c1ade180e2362e26aa222ca5837ad5e56
Abhinav <pandeysandeep1190@gmail.com>
2022-01-31 12:32:13 +0530

    Kardiya

--- frontend/src/components/portfolio-landing/portfolio-landing.js
+++ frontend/src/components/portfolio-landing/portfolio-landing.js
@@ -4,94 +4,23 @@ import ashu from "../../imgs/god/ashutosh.jpeg";
 import astro from "../../imgs/astro.png";
 import { ButtonGroup, InputGroup, FormControl } from "react-bootstrap";
 import Navbar from "../navbar/navbar"
+import {Link} from 'react-router-dom';
 import {connect} from 'react-redux'
 import * as actions from '../../actions/index'
 
 const Landing = (props) => {
   const [name, SetName] = useState("");
-  
+  const [data,setData] = useState([])
   useEffect(()=>{
     props.getAllUsers()
   },[])
 
-  let data = [
-    {
-      img: ashu,
-      membername: "Rtr. Ashutosh Upadhyay",
-      status: "President",
-    },
-    {
-      img: ashu,
-      membername: "Amaan shaikh",
-      status: "President",
-    },
-    {
-      img: ashu,
-      membername: "Aakash Varma",
-      status: "President",
-    },
-    {
-      img: ashu,
-      membername: "Sanloni sharma",
-      status: "President",
-    },
-    {
-      img: ashu,
-      membername: "Rajesh ulhas",
-      status: "President",
-    },
-    {
-      img: ashu,
-      membername: "Rajesh ulhas",
-      status: "President",
-    },
-    {
-      img: ashu,
-      membername: "Rajesh ulhas",
-      status: "President",
-    },
-    {
-      img: ashu,
-      membername: "Rajesh ulhas",
-      status: "President",
-    },
-    {
-      img: ashu,
-      membername: "Rajesh ulhas",
-      status: "President",
-    },
-    {
-      img: ashu,
-      membername: "Rajesh ulhas",
-      status: "President",
-    },
-    {
-      img: ashu,
-      membername: "Rajesh ulhas",
-      status: "President",
-    },
-    {
-      img: ashu,
-      membername: "Rajesh ulhas",
-      status: "President",
-    },
-    {
-      img: ashu,
-      membername: "Rajesh ulhas",
-      status: "President",
-    },
-    {
-      img: ashu,
-      membername: "Rajesh ulhas",
-      status: "President",
-    },
-    {
-      img: ashu,
-      membername: "Rajesh ulhas",
-      status: "President",
-    },
-  ];
-  console.log(props)
+  useEffect(()=>{
+    if(props.users){
+      setData(props.users.userData)
+    }
+  },[props])
+ 
   return (
     <div className="main-11">
       <Navbar flags={1} />
@@ -111,28 +40,35 @@ const Landing = (props) => {
         </div>
       </div>
       <div className="landing-containers">
-        {data
-          .filter((item) => {
-            if (item.membername === "") {
-              return item;
-            } else if (
-              item.membername.toLowerCase().includes(name) ||
-              item.membername.includes(name)
-            ) {
-              return item;
-            }
-          })
-          .map((member, i) => {
-            return (
-              <div className="member-div-1" key={i}>
-                <div className="member-div-1-div">
-                  <img src={member.img} className="member-div-1-image"></img>
+        {data && data.length > 0 ? 
+          <div>
+            {
+          
+          data.filter((item) => {
+              if (item.full_name === "") {
+                return item;
+              } else if (
+                item.full_name.toLowerCase().includes(name) ||
+                item.full_name.includes(name)
+              ) {
+                return item;
+              }
+            })
+            .map((member, i) => {
+              return (
+                <div className="member-div-1" key={i}>
+                  <Link to={`/portfolio/${member.full_name}`}>
+                  <div className="member-div-1-div">
+                    <img src={member.img} className="member-div-1-image"></img>
+                  </div>
+                  <div className="member-div-1-name">{member.full_name}</div>
+                  <div className="member-div-1-status">{member.current_position}</div>
+                  </Link>
                 </div>
-                <div className="member-div-1-name">{member.membername}</div>
-                <div className="member-div-1-status">{member.status}</div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
+            :(null)}
       </div>
     </div>
   );
